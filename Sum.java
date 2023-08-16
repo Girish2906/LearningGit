cb0f2c4d009dd3c99290222bb34fb7f17bf526ed
Girish Bhargava <girishbhargava9202@gmail.com>
2023-08-17 05:07:36 +0530

    initial message

--- Sum.java
+++ Sum.java
@@ -2,5 +2,7 @@ public class Sum{
     public static void main(String args[]){
         int a  = 10 , b = 20; 
         System.out.println(a + b) ; 
+        int c = 1200 , g = 13 ; 
+        System.out.println(c + g) ; 
     }
 }
