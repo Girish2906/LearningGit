e53aa8b5f3dd34997ee07d7b5a8e8c412c50b5a9
Girish Bhargava <girishbhargava9202@gmail.com>
2023-08-17 06:04:28 +0530

    managed multiply class in the file structure

---
+++ multiply.java
@@ -0,0 +1,7 @@
+public class Sum{
+    public static void main(String args[]){
+        int a  = 10 , b = 20; 
+        System.out.println(a * b) ; 
+        System.out.println("This is multiply class written by the best developer of all times") ; 
+    }
+}
